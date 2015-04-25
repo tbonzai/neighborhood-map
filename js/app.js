1a6bcb9b9f1b3b1f63ad891c293cfc3a97536542
Tom Benzaia <tbenzaia@gmail.com>
2015-04-24 21:01:16 -0500

    Got the map and yelp images working right.

--- js/app.js
+++ js/app.js
@@ -2,10 +2,16 @@
 var Location = function(locationData) {
 	var self = this;
 
-	this.visible = ko.observable(true);
+	this.isHidden = ko.observable(false);
 	this.isSelected = ko.observable(false);
 	this.name = ko.observable(locationData.name);
 	this.title = ko.observable(locationData.title);
+	this.yelpImgSrc = ko.observable('');
+	this.yelpAltText = ko.observable('');
+	this.yelpReviewText = ko.observable('');
+	this.hasYelp = ko.computed(function() {
+		return self.yelpImgSrc().length > 0;
+	});
 	this.phone = ko.observable(locationData.phone);
 	this.type = ko.observable(locationData.type);
 	this.position = locationData.position;
@@ -25,31 +31,40 @@ var Location = function(locationData) {
 			phone: self.phone,
 			ywsid: 'xuFaz9OB2ls4CoiJXqqqCA'
 		},
-		error: function(e){
-			console.log('error');
-		},
-		success: function(data) {
-//			console.log('success');
-//			console.log(data);
+		error: function(ex) {
+			console.log('error')
 		}
 	})
-	.done(function() {
-//		console.log("done");
+	.done(function(data) {
+		try {
+			if (data.message.code == 0 && data.businesses.length > 0) {
+				self.yelpImgSrc(data.businesses[0].rating_img_url_small);
+				self.yelpAltText('Yelp ' + data.businesses[0].avg_rating + ' star rating image.');
+				if (data.businesses[0].reviews.length > 0) {
+					self.yelpReviewText(data.businesses[0].reviews[0].text_excerpt);
+				}
+			} else {
+				self.doYelpError();
+			}
+		}
+		catch (ex) {
+			self.doYelpError();
+		}
 	})
 	.fail(function() {
-//		console.log("fail");
-	})
-	.always(function() {
-//		console.log("always");
+		self.doYelpError();
 	});
 
+	this.doYelpError = function() {
+		ViewModel.setErrorMessage('An error occurred while retrieving Yelp data.');
+	};
 
 	google.maps.event.addListener(self.marker, 'click', function() {
 		ViewModel.changeLocation(this.koObject);
 	});
 
 	this.doClick = function(forceDisable) {
-		if (self.marker.getAnimation() != null || forceDisable) {
+		if (self.marker.getAnimation() != null || forceDisable === true) {
 			self.marker.setAnimation(null);
 			self.isSelected(false);
 			mapObject.setZoom(15);
@@ -61,26 +76,19 @@ var Location = function(locationData) {
 			mapObject.setCenter(self.position);
 		}
 	};
-
-	this.showName = function() {
-		return self.name();
-	};
 };
 
 var ViewModelFunc = function(){
 
 	var	self = this;
 
-	this.errorExists = ko.observable(false);
-
 	this.locationList = ko.observableArray([]);
 
-	this.errorMessage = ko.observable(
-		'<button type="button" class="close" data-dismiss="alert" aria-label="Close">' +
-			'<span aria-hidden="true">&times;</span>' +
-		'</button>' +
-		'<strong>Sorry!</strong> An issue occurred while trying to retrieve Yelp reviews.'
-	);
+	this.errorMessage = ko.observable('');
+
+	this.noError = ko.computed(function() {
+		return self.errorMessage().length <= 0;
+	});
 
 	this.currentLocation = ko.observable(null);
 
@@ -89,6 +97,29 @@ var ViewModelFunc = function(){
 	});
 
 	this.currentLocation = ko.observable(self.locationList()[0]);
+
+	this.setErrorMessage = function(errorMessage) {
+		self.errorMessage(errorMessage);
+	};
+
+	this.doSearch = function(x, z) {
+		var searchTarget = z.target.value.toLowerCase();
+		var itemTitle;
+		var itemType;
+		for (var i = 0; i < self.locationList().length; i++) {
+			itemTitle = self.locationList()[i].title().toLowerCase();
+			itemType = self.locationList()[i].type().toLowerCase();
+			if (itemTitle.indexOf(searchTarget) > -1 || itemType.indexOf(searchTarget) > -1) {
+				self.locationList()[i].isHidden(false);
+			} else {
+				self.locationList()[i].isHidden(true);
+				if (self.locationList()[i].isSelected() === true) {
+					self.locationList()[i].doClick(true);
+				}
+			}
+		}
+		return true;
+	};
 
 	this.changeLocation = function(locationObject) {
 		if (self.currentLocation() != locationObject) {
